08792a97eb77bfbb50f745244d737fafd6edab5b
vasya000 <vasya@gmail.com>
2024-12-09 19:15:39 +0200

    12

--- function.cpp
+++ function.cpp
@@ -1,1 +1,37 @@
-1
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    char c;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b;
+    cout << "Enter an operator: ";
+    cin >> c;
+
+    int* pa = &a;
+    int* pb = &b;
+    char* pc = &c;
+
+    if (*pc == '+') {
+        cout << *pa << "+" << *pb << "=" << *pa + *pb << endl;
+    }
+    else if (*pc == '-') {
+        cout << *pa << "-" << *pb << "=" << *pa - *pb << endl;
+    }
+    else if (*pc == '*') {
+        cout << *pa << "*" << *pb << "=" << *pa * *pb << endl;
+    }
+    else if (*pc == '/') {
+        cout << *pa << "/" << *pb << "=" << *pa / *pb << endl;
+    }
+    else {
+        cout << "Invalid operator" << endl;
+    }
+
+    return 0;
+}
