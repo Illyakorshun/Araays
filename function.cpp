820ac7a4e8797d5b9c2dbb8150d5002aa6cfaf78
vasya000 <vasya@gmail.com>
2024-12-09 19:13:50 +0200

    12

--- function.cpp
+++ function.cpp
@@ -1,55 +1,30 @@
-#include <iomanip>
 #include <iostream>
-#include <random>
 using namespace std;
 
 int main() {
-    random_device rd;
-    mt19937 mersenne(rd());
-    uniform_int_distribution<int> uid(0, 10);
 
-    const int size = 5;
-    int array[size][size];
+    int a;
+    int b;
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            array[i][j] = uid(mersenne);
-            cout << setw(3) << array[i][j] << " ";
-        }
-        cout << endl;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    if (a > b) {
+        int* pb = &b;
+        cout << a << " > " << *pb << endl;
     }
-
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            sum += array[i][j];
-        }
+    else if (a < b) {
+        int* pa = &a;
+        cout << b << " > " << *pa << endl;
     }
-
-    int median = sum / size;
-
-    int max = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (array[i][j] > max) {
-                max = array[i][j];
-            }
-        }
+    else {
+        cout << a << " = " << b << endl;
     }
-
-    int min = array[0][0];
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (array[i][j] < min) {
-                min = array[i][j];
-            }
-        }
-    }
-
-    cout << "Sum of all elements is " << sum << endl;
-    cout << "Median of all elements is " << median << endl;
-    cout << "Max of all elements is " << max << endl;
-    cout << "Min of all elements is " << min << endl;
 
     return 0;
 }
